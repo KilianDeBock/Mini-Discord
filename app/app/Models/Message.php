517966dfbda1365caf5aae1f3ba567eee8a9f258
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-10-28 12:00:39 +0200

    22.43.5.5: Add relative time for message

--- app/app/Models/Message.php
+++ app/app/Models/Message.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
@@ -38,5 +39,45 @@ class Message extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    private function _formatedDate($_date)
+    {
+        $now = time();
+        $date = strtotime($_date);
+        $diff = $now - $date;
+
+        if ($diff < 60) {
+            return sprintf($diff > 1 ? '%s seconds ago' : 'a second ago', $diff);
+        }
+
+        $diff = floor($diff / 60);
+
+        if ($diff < 60) {
+            return sprintf($diff > 1 ? '%s minutes ago' : 'one minute ago', $diff);
+//            return __('one minute ago | %s minutes ago', [$diff]);
+        }
+
+        $diff = floor($diff / 60);
+
+        if ($diff < 24) {
+            return sprintf($diff > 1 ? '%s hours ago' : 'an hour ago', $diff);
+        }
+
+        $diff = floor($diff / 24);
+
+        if ($diff < 7) {
+            return sprintf($diff > 1 ? '%s days ago' : 'yesterday', $diff);
+        }
+
+        return strftime('%A %e %B %Y %H:%M:%S', $date);
+    }
+
+    protected function formatedDate(): Attribute
+    {
+        $string = $this->_formatedDate($this->created_at);
+        return Attribute::make(
+            get: fn($value) => $string,
+        );
     }
 }
--- app/resources/views/message/message.blade.php
+++ app/resources/views/message/message.blade.php
@@ -8,15 +8,25 @@
                 <span class="message__reply__username">{{ $replyMessage->user->username }}</span>
                 <span class="message__reply__content">{{ $replyMessage->content }}</span>
             </section>
+        @else
+            <section class="message__reply">
+                <img class="message__reply__avatar"
+                     src="{{ asset("storage/users/avatars/0.png") }}" alt="Avatar">
+                <span class="message__reply__username">Unkown</span>
+                <span class="message__reply__content">Message has been deleted</span>
+            </section>
         @endif
     @endif
     <section class="message__info">
         <img class="message__avatar" src="{{ asset("storage/users/avatars/{$message->user->avatar_url}") }}"
              alt="Avatar">
         <span class="message__username">{{ $message->user->username }}</span>
-        <span class="message__date">{{ $message->created_at }}</span>
+        <span class="message__date">{{ $message->formated_date }}</span>
     </section>
     <p class="message__content">
         @include('message.messageContent')
     </p>
+    @if($guild->user_id == Auth::user()->id)
+        @include('message.messageDelete')
+    @endif
 </li>
